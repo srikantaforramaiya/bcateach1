1ed3dc4e1c7daafffec485a3f32a4acf20827b85
srikantaforramaiya <srikantaforramaiya@gmail.com>
2026-09-22 08:32:15 +0530

    added files of Python
    added files of Python

---
+++ for-loop.py
@@ -0,0 +1,17 @@
+# Loop from 0 up to (but not including) 5
+for i in range(5):
+    print(i)
+	
+	
+# Loop from 2 up to (but not including) 6
+for i in range(2, 6):
+    print(i)
+	
+
+# Loop from 1 up to (but not including) 10, incrementing by 2 (odd numbers)
+for i in range(1, 10, 2):
+    print(i)
+	
+	
+for i in range(5, 0, -1):
+    print(i)
